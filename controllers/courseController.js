02141ca6ea7913700e8d589f9ab48bffc21ce808
KaraElBatson <210316035@ogr.cbu.edu.tr>
2023-11-07 19:21:33 +0300

    Kurs-Öğrenci part2
    1Kursta kayıtlı isek enroll butonu yok onun yerine dashboardda release butonu var
    2üye yoksa enroll butonu yok
    3kutu açıklaması  uzun ise substring ile ayarlama

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -63,12 +63,14 @@ exports.getAllCourses = async (req, res) => {
 // tek kurs alinarak kurs detay sayfasina gitmesi icin fonksiyon
 exports.getCourse = async (req, res) => {
   try {
+    const user = await User.findById(req.session.userID);
     // kurs sayfasi slug ile bulundu slugify paketi ile database e kaydedilen veri de slug otomaik olusturuldu
     const course = await Course.findOne({
       slug: req.params.slug,
     }).populate('user');
     res.status(200).render('course', {
       course,
+      user,
       page_name: 'courses',
     });
   } catch (error) {
@@ -98,6 +100,25 @@ exports.enrollCourse = async (req, res) => {
     //   course,
     //   page_name: 'courses',
     // });
+    res.redirect('/users/dashboard');
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
+// kurs kaydini silmek icin olusturulan fonksiyon
+exports.releaseCourse = async (req, res) => {
+  try {
+    const user = await User.findById(req.session.userID);
+
+    // pull ile veritabanindan silinmesi saglandi
+    await user.courses.pull({
+      _id: req.body.course_id,
+    });
+    await user.save();
+
     res.redirect('/users/dashboard');
   } catch (error) {
     res.status(400).json({
